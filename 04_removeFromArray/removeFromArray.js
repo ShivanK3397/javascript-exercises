b99b44b19f5bd77c7976a8d698b4cb6997b032ff
Shivan Kathir <shivan.kathir@gmail.com>
2024-05-18 01:21:18 -0400

    finished exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removeValues) {
+    const arrayRemoveValues =Array.from(removeValues);
+    return array.filter((value)=>(!arrayRemoveValues.includes(value)));
+    
 };
 
 // Do not edit below this line
