ed3c225fd4e70903cf6536004bd669b4c56a971b
Shivan Kathir <shivan.kathir@gmail.com>
2024-05-18 01:40:07 -0400

    finished exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function(start,end) {
+    if(typeof start!=="number"||typeof end!=="number"||start<0||end<0){
+        return "ERROR";
+    }
+
+    if(start>end){
+        tempStart=start;
+        tempEnd=end;
+        start=tempEnd;
+        end=tempStart;
+    }
+    let i =start;
+    let sum=0;
+    while(i<=end){
+        sum+=i;
+        i++;
+
+    }
+    return sum;
 
 };
 
