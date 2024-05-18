dc09dff3313f3077933c75db3a61dfa0377248dc
Shivan Kathir <shivan.kathir@gmail.com>
2024-05-18 00:40:57 -0400

    finished exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,9 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const arrayReverseString=[];
+    for (let i = 0;i<string.length;i++){
+        arrayReverseString[i]=string[string.length-1-i];
+    }
+    return arrayReverseString.join(" ")
 
 };
 
